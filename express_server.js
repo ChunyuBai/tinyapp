225129c1cc7587f0f58f9c8c76f3458fbf1870c4
ChunyuBai <baichunyu26@gmail.com>
2022-11-30 19:20:23 -0500

    add a registration for our app

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cookieParser = require('cookie-parser');
 const app = express();
+const morgan = require('morgan')
+
 const PORT = 8080; // default port 8080
 
 //Installing and Setting Up EJS
@@ -9,13 +11,46 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());//cookie use
-
+app.use(morgan('dev'));
 
 // create a url database
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+// create our user database
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+//create a lookup function for register
+const lookupHelper = (email) => {
+  for(let key in users){
+    if(users[key].email === email) {
+      return users[key];
+    } 
+  }    
+  return null;
+}
+//create a look up function for login
+const lookupLogin = (email,password) => {
+  for(let key in users) {
+    if(users[key].email === email && users[key].password === password){
+      return users[key];
+    }
+  } return null;
+}
+
+
 //Generate a Random Short URL ID
 const generateRandomString = () => {
   return Math.random().toString(20).substring(2,8);
@@ -32,8 +67,10 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
+  const userId = req.cookies["userId"];
+  const user = users[userId];
   const templateVars = { 
-    username: req.cookies["userName"],
+    user: user,
     urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
@@ -61,19 +98,52 @@ app.get("/set", (req, res) => {
 
 //Add a GET Route to Show the Form
  app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+    const userId = req.cookies["userId"];
+    const user = users[userId];
+  res.render("urls_new",{
+    user:user,
+  });
 });
+
+//go to the register site
+app.get("/register",(req,res) => {
+  const templateVars = {
+    user: null,
+  }
+  res.render("urls_register",templateVars);
+})
+
+//Create a Registration Handler
+app.post("/register",(req,res) => {
+  const userID = generateRandomString();
+  const {email} = req.body;
+  const {password} = req.body;
+  const result = lookupHelper(email);
+  if(email.trim() === "" ){
+    res.status(400).send("empty email")
+  } 
+   else if(result){
+    res.status(400).send("email has been used")
+  } else {
+    users[userID] = {id:userID,email,password};
+  }
+  res.cookie('userId',userID);
+  res.redirect("/urls");
+  console.log(users);
+  console.log(result);
+})
+
 
 //Add a POST Route to Receive the Form Submission
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
-  // urldabase = { 'abc123': 'facebook.com"}
   const longURL = req.body.longURL;
   urlDatabase[shortURL] = longURL;
   console.log(urlDatabase);
   console.log(req.body); // Log the POST request body to the console/
   res.redirect('/urls/:id');//Redirect After Form Submission
 });
+
 
 //Delete a url
 app.post("/urls/:id/delete", (req, res) => {
@@ -84,18 +154,29 @@ app.post("/urls/:id/delete", (req, res) => {
 
 
 //login a user
+app.get("/login",(req,res) => {
+  const templateVars = {
+    user: null,
+  }
+  res.render("login", templateVars)
+})
+
 app.post("/login", (req, res) => {
   // const username = req.body.username;
-  const {username} = req.body;
-  // Set-Cookie: userName = userName;
-  res.cookie('userName',username)
-  // console.log(userName)
-  console.log('Cookies: ', req.cookies["userName"])
+  
+  const {email} = req.body;
+  const {password} = req.body;
+  const user = lookupLogin(email,password)
+  if(user){
+    res.cookie('userId',user.id) 
+  } else {
+    res.status(403).send("not found user")
+  }
   res.redirect('/urls');//Redirect After Form Submission
 });
 //logout a user
 app.post("/logout",(req, res) => {
-  res.clearCookie("userName");
+  res.clearCookie("userId");
   res.redirect('/urls');
 })
 
